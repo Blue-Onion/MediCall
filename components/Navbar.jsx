c2e63c716b503195513de0b93cd9676ccc46ebab
Blue Onion <rawatadityasingh963@gmail.com>
2025-06-16 23:33:23 +0530

    User

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,10 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import { Button } from "./ui/button";
-const Navbar = () => {
+import { checkUser } from "@/lib/checkuser";
+import { Stethoscope, User } from "lucide-react";
+const Navbar = async () => {
+  const user = await checkUser();
   return (
     <header className="fixed top-0 bg-background/60 border border-b backdrop-blur-lg w-full z-100">
       <nav className=" container max-w-none mx-auto flex items-center justify-between py-4 px-6">
@@ -22,7 +25,35 @@ const Navbar = () => {
             className="cursor-pointer"
           />
         </Link>
-        <div className="">
+        <div className="flex items-center gap-5 space-x-4">
+          <SignInButton>
+            <div className="">
+              {user.role === "UNASSIGNED" && (
+                <Button variant={"outline"} className="mr-2">
+                  <Link href={"/onboarding"}>
+                    <div className="flex gap-2">
+                      <User className="h-4 w-4" />
+                      <span className="hidden md:inline-block">
+                        Complete Profile
+                      </span>
+                    </div>
+                  </Link>
+                </Button>
+              )}
+              {user.role === "DOCTOR" && (
+                <Button variant={"seconadry"} className="mr-2">
+                  <Link href={"/doctor"}>
+                    <div className="flex gap-2">
+                      <Stethoscope className="h-4 w-4" />
+                      <span className="hidden md:inline-block">
+                        Complete Profile
+                      </span>
+                    </div>
+                  </Link>
+                </Button>
+              )}
+            </div>
+          </SignInButton>
           <SignedOut>
             <SignInButton>
               <Button variant={"secondary"}>Sign In</Button>
